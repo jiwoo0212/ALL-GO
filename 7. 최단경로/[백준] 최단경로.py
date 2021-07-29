f8c02d6569f04031a22b792fc8227a3a3e8d9f4e
jiwoo0212 <68782183+jiwoo0212@users.noreply.github.com>
2021-07-30 00:16:31 +0900

    Create 최단경로.py

--- 7. 최단경로/[백준] 최단경로.py
+++ 7. 최단경로/[백준] 최단경로.py
@@ -1,51 +1,41 @@
-# 이코테 참고
+# 이코테 코드 참고
+
 import heapq
 import sys
 input = sys.stdin.readline
-INF = int(1e9) # 무한을 의미하는 값으로 10억을 설정
+INF = int(1e9)
 
-# 노드의 개수, 간선의 개수를 입력받기
 n, m = map(int, input().split())
-# 시작 노드 번호를 입력받기
-start = int(input())
-# 각 노드에 연결되어 있는 노드에 대한 정보를 담는 리스트를 만들기
-graph = [[] for i in range(n + 1)]
-# 최단 거리 테이블을 모두 무한으로 초기화
-distance = [INF] * (n + 1)
+s = int(input())
 
-# 모든 간선 정보를 입력받기
+# 노드의 연결들
+graph = [[] for i in range(n+1)]
+# 확정된 최단거리 저장
+distance = [INF]*(n+1)
+
 for _ in range(m):
     a, b, c = map(int, input().split())
-    # a번 노드에서 b번 노드로 가는 비용이 c라는 의미
+    # 해당노드와 (연결노드, 비용)
     graph[a].append((b, c))
 
-def dijkstra(start):
+def dijkstra(s):
     q = []
-    # 시작 노드로 가기 위한 최단 경로는 0으로 설정하여, 큐에 삽입
-    heapq.heappush(q, (0, start))
-    distance[start] = 0
-    while q: # 큐가 비어있지 않다면
-        # 가장 최단 거리가 짧은 노드에 대한 정보 꺼내기
-        dist, now = heapq.heappop(q)
-        # 현재 노드가 이미 처리된 적이 있는 노드라면 무시
+    heapq.heappush(q, (0, s)) # 우선순위큐로 만들고 (비용, 노드번호) append
+    distance[s] = 0
+    while q:
+        dist, now = heapq.heappop(q) # 가장 비용이 적은 노드가 뽑힌다
         if distance[now] < dist:
             continue
-        # 현재 노드와 연결된 다른 인접한 노드들을 확인
-        for i in graph[now]:
+        for i in graph[now]: # now(현재노드)와 연결된 노드들을 순서대로 본다
             cost = dist + i[1]
-            # 현재 노드를 거쳐서, 다른 노드로 이동하는 거리가 더 짧은 경우
-            if cost < distance[i[0]]:
+            if cost < distance[i[0]]: # 만약 i를 거쳐서 가는 비용이 더 적다면 갱신
                 distance[i[0]] = cost
-                heapq.heappush(q, (cost, i[0]))
+                heapq.heappush(q, (cost, i[0])) # (새로 갱신된 비용, 노드번호)
 
-# 다익스트라 알고리즘을 수행
-dijkstra(start)
+dijkstra(s)
 
-# 모든 노드로 가기 위한 최단 거리를 출력
-for i in range(1, n + 1):
-    # 도달할 수 없는 경우, 무한(INFINITY)이라고 출력
+for i in range(1, n+1):
     if distance[i] == INF:
-        print("INF")
-    # 도달할 수 있는 경우 거리를 출력
+        print('INF')
     else:
         print(distance[i])
